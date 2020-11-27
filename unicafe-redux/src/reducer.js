d45f258ec626342b56fbda76e7b9d2eb725991ab
Chloe-Le97 <phuonganhkitty365@gmail.com>
2020-11-27 03:56:39 +0200

    complete unicafe-redux part

---
+++ unicafe-redux/src/reducer.js
@@ -0,0 +1,34 @@
+const initialState = {
+  good: 0,
+  ok: 0,
+  bad: 0
+}
+
+const counterReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case 'GOOD':
+      let newGood = state.good + 1;
+      return {
+        ...state,
+        good: newGood
+      }
+    case 'OK':
+      let newOk = state.ok + 1;
+      return {
+        ...state,
+        ok: newOk
+      }
+    case 'BAD':
+      let newBad = state.bad + 1;
+      return {
+        ...state,
+        bad: newBad
+      }
+    case 'ZERO':
+      return initialState
+    default: return state
+  }
+  
+}
+
+export default counterReducer
